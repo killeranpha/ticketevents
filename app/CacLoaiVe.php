baf3c94ca61019d7417b73ee2c7838ad25493d75
= <=>
2018-07-23 10:30:41 +0700

    23-7-2018_HieuND_model

---
+++ app/CacLoaiVe.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CacLoaiVe extends Model
+{
+    protected $table = 'cacloaive';
+}
---
+++ app/DonVi.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DonVi extends Model
+{
+    protected $table = 'donvi';
+}
