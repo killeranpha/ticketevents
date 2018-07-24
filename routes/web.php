acac6fc86fe6b9f5db89d90620d38cd9845e258b
= <=>
2018-07-24 20:58:24 +0700

    web-route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,36 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/abc', function () {
+    return view('welcome');
+});
+Route::get('/index', function () {
+    return view('admin.layout.index');
+});
+Route::get('/home', function () {
+    return view('admin.layout.home');
+});
+Route::get('/danhsach', function () {
+    return view('admin.event.danhsach');
+});
+Route::get('/taomoi', function () {
+    return view('admin.event.taomoi');
+});
+Route::get('/login', function () {
+    return view('admin.layout.login');
+});
+Route::get('/forget-pass', function () {
+    return view('admin.layout.forget-pass');
+});
+Route::get('/ganday', function () {
+    return view('admin.event.ganday');
+});
+Route::get('/thongke', function () {
+    return view('admin.thongke.thongke');
+});
+Route::get('/danhsachnguoidung', function () {
+    return view('admin.nguoidung.danhsachnguoidung');
+});
+Route::get('/quanlynguoidung', function () {
+    return view('admin.nguoidung.quanlynguoidung');
+});
